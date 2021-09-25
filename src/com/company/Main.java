6edb621fcd2a0095643354aca348be1967b169ae
Kenny <Superkennymon123@gmail.com>
2021-09-24 20:00:19 -0700

    Yandere Dev Sim

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
+    String word = null;
+    String word_2 = null;
+    String woord = null;
 
     GregorianCalendar cal = new GregorianCalendar();
     GregorianCalendar birth = new GregorianCalendar(2005, Calendar.OCTOBER, 22);
@@ -19,10 +22,55 @@ public class Main {
     int weekday = cal.get(Calendar.DAY_OF_WEEK);
     int year = cal.get(Calendar.YEAR);
     int month = cal.get(Calendar.MONTH);
-    System.out.println(month + 1 + "/" + day + "/" + year + " and weekday is " + weekday);
+
+        if (weekday == 1) {
+            woord = "Sunday";
+        }
+        else if (weekday == 2) {
+            woord = "Monday";
+        }
+        else if (weekday == 3) {
+            woord = "Tuesday";
+        }
+        else if (weekday == 4) {
+            woord = "Wednesday";
+        }
+        else if (weekday == 5) {
+            woord = "Thursday";
+        }
+        else if (weekday == 6) {
+            woord = "Friday";
+        }
+        else if (weekday == 7) {
+            woord = "Saturday";
+        }
+
+        System.out.println(month + 1 + "/" + day + "/" + year + " and weekday is " + woord);
 
     int birthday = birth.get(Calendar.DAY_OF_WEEK);
-    System.out.println(birthday);
+
+        if (birthday == 1) {
+            word = "Sunday";
+        }
+        else if (birthday == 2) {
+            word = "Monday";
+        }
+        else if (birthday == 3) {
+            word = "Tuesday";
+        }
+        else if (birthday == 4) {
+            word = "Wednesday";
+        }
+        else if (birthday == 5) {
+            word = "Thursday";
+        }
+        else if (birthday == 6) {
+            word = "Friday";
+        }
+        else if (birthday == 7) {
+            word = "Saturday";
+        }
+        System.out.println(word);
 
     birth.add(Calendar.DAY_OF_MONTH, 10000);
 
@@ -30,7 +78,31 @@ public class Main {
     int weekday_2 = birth.get(Calendar.DAY_OF_WEEK);
     int year_2 = birth.get(Calendar.YEAR);
     int month_2 = birth.get(Calendar.MONTH);
-    System.out.println(month_2 + 1 + "/" + day_2 + "/" + year_2 + " and weekday is " + weekday_2);
+
+    if (weekday_2 == 1) {
+        word_2 = "Sunday";
+    }
+    else if (weekday_2 == 2) {
+        word_2 = "Monday";
+    }
+    else if (weekday_2 == 3) {
+        word_2 = "Tuesday";
+    }
+    else if (weekday_2 == 4) {
+        word_2 = "Wednesday";
+    }
+    else if (weekday_2 == 5) {
+        word_2 = "Thursday";
+    }
+    else if (weekday_2 == 6) {
+        word_2 = "Friday";
+    }
+    else if (weekday_2 == 7) {
+        word_2 = "Saturday";
+    }
+
+
+    System.out.println(month_2 + 1 + "/" + day_2 + "/" + year_2 + " and weekday is " + word_2);
 
 
 
